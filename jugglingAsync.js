728d9053e41437738ef1d87d34589374571702cb
David Wickes <dave.wickes@gmail.com>
2014-12-02 17:25:50 +0000

    refactors juggling async to make it a little more generic

--- jugglingAsync.js
+++ jugglingAsync.js
@@ -1,35 +1,27 @@
-
 var http = require('http');
-var urls = [process.argv[2], process.argv[3], process.argv[4]]
-var result  = ["", "", ""];
-var flags = [false, false, false];
+var urls = process.argv.slice(2);
+var results = [];
+var streamsComplete = 0;
+
+function printResults(results) {
+  results.forEach(function (data) {
+    console.log(data);
+  })
+}
 
 urls.forEach(function (url, index) {
   http.get(url, function (response) {
-    _result = "";
     response.setEncoding('utf8');
     response.on('data', function(data) {
-      result[index] += data.toString();
+      if (results[index]) {results[index] += data.toString();}
+      else {results[index] = data.toString();}
     });
     response.on('error', console.error);
     response.on('end', function() {
-      flags[index] = true;
-      if (flags.indexOf(false) == -1) {
-        result.forEach(function (data) {
-          console.log(data);
-        })
+      streamsComplete ++;
+      if (streamsComplete == urls.length) {
+        printResults(results);
       }
     });
   });
 });
-
-//loop1:
-  //for ( ; ; ) {
-    //while (result.indexOf(false)) {
-      //result.forEach(function (data) {
-        //console.log(data);
-      //})
-      //break loop1
-    //}
-  //}
-
